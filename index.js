bcda09df084cb79033617fdd575bb740fa84e921
AndiKod <andrei@andikod.fr>
2022-10-26 00:01:41 +0200

    E:/Programmes/Git/api/users

--- index.js
+++ index.js
@@ -5,38 +5,45 @@ const User = require('./User');
 sequelize.sync({ force: true }).then(() => console.log('db is ready'));
 
 const app = express();
-
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
+
+// --- API Routes --- //
+// CRATE user
+app.post('/api/users', async (req, res) => {
   await User.create(req.body);
   res.send("success");
 })
-
-app.get('/users', async (req, res) => {
+// GET all users
+app.get('/api/users', async (req, res) => {
   const users = await User.findAll();
   res.send(users);
 })
-
-app.get('/users/:id', async (req, res) => {
+// GET one user
+app.get('/api/users/:id', async (req, res) => {
   const id = req.params.id;
   const user = await User.findOne({where: {id: id}});
   res.send(user);
 })
-
-app.put('/users/:id', async (req, res) => {
+// UPDATE one user
+app.put('/api/users/:id', async (req, res) => {
   const id = req.params.id;
   const user = await User.findOne({where: {id: id}});
   user.username = req.body.username;
   await user.save();
   res.send('updated');
 })
-
-app.delete('/users/:id', async (req, res) => {
+// DELETE one user
+app.delete('/api/users/:id', async (req, res) => {
   const id = req.params.id;
   await User.destroy({where: {id: id}});
   res.send('removed');
 })
+
+app.get('/', (req, res) => {res.send('<h1>Kiwi</h1>')})
+
+
+
 
 app.listen(3000, () => {
   console.log("app is running");
